a33a65f13d4192cd0b6a1bccfe78fce43e709fa2
Adhitya Bagasmiwa Permana <adhitya.bagasmiwa@bni.co.id>
2024-01-22 17:44:49 +0700

    feature: add checking data from db when save movie db and add delete movie db

--- ios-foundation/LocalDB/DatabaseManager.swift
+++ ios-foundation/LocalDB/DatabaseManager.swift
@@ -14,6 +14,19 @@ class DatabaseManager: NSObject {
         let context = CoreDataContainer.persistentContainer.viewContext
         let newMovie = MovieDB(context: context)
         
+        do {
+            let moviesDb: [MovieDB] = try context.fetch(MovieDB.fetchRequest())
+            
+            if moviesDb.count > 0 && moviesDb.contains(where: { $0.id == movie.id ?? .zero }) {
+                if let movie = moviesDb.first(where: { $0.id == movie.id ?? .zero }) {
+                    return
+                }
+            }
+        } catch let error {
+            debugPrint("[LOG - Error Check From DB Core Data]: ", error)
+            return
+        }
+        
         newMovie.id = Int64(movie.id ?? .zero)
         newMovie.name = movie.title
         newMovie.voteAverage = movie.voteAverage ?? .zero
@@ -22,10 +35,10 @@ class DatabaseManager: NSObject {
         
         CoreDataContainer.saveContext(completion: { result in
             switch result {
-                case .success(let isSuccess):
-                    completion(.success(isSuccess))
-                case .failure(let error):
-                    completion(.failure(error))
+            case .success(let isSuccess):
+                completion(.success(isSuccess))
+            case .failure(let error):
+                completion(.failure(error))
             }
         })
     }
@@ -61,6 +74,24 @@ class DatabaseManager: NSObject {
             completion(.success(movies))
         } catch let error {
             debugPrint("[LOG - Error Get Core Data]: ", error)
+            completion(.failure(error))
+        }
+    }
+    
+    func deleteMovieFromDb(movieId: Int, completion: @escaping ((Result<Bool, Error>) -> Void)) {
+        let context = CoreDataContainer.persistentContainer.viewContext
+        
+        do {
+            let moviesDb: [MovieDB] = try context.fetch(MovieDB.fetchRequest())
+            
+            if moviesDb.count > 0 && moviesDb.contains(where: { $0.id == movieId }) {
+                if let movie = moviesDb.first(where: { $0.id == movieId }) {
+                    context.delete(movie)
+                    completion(.success(true))
+                }
+            }
+        } catch let error {
+            debugPrint("[LOG - Error Delete Core Data]: ", error)
             completion(.failure(error))
         }
     }
