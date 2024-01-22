a11a2a46557f72141a3f4238a178e31dfa1c0bd2
Erwin Ramadhan <erwin.ramadhan.edwar@bni.co.id>
2024-01-22 12:24:02 +0700

    Initial setup

---
+++ ios-foundation/Scene/HomeViewController.swift
@@ -0,0 +1,29 @@
+//
+//  HomeViewController.swift
+//  ios-foundation
+//
+//  Created by Erwin Ramadhan Edwar Putra on 22/01/24.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
